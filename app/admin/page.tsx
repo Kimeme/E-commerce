ece5e1f8ebaaad35b3b7a3e53669fed6bf77f532
Kimeme <yemisrachmag@gmail.com>
2026-02-12 17:19:30 +0300

    Added Summary and formatNumber file

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,22 @@
-const Admin = () =>{
-    return (<div className="pt-8">Admin Page</div>);
+
+import Summary from "./Summary";
+import getProducts from "@/actions/getProducts";
+import getOrders from "@/actions/getOrders";
+import getUsers from "@/actions/getUsers";
+import { Container } from "@mui/material";
+
+const Admin = async() =>{
+    const products= await getProducts({category:null})
+    const orders = await getOrders()
+    const users = await getUsers()
+
+
+    return (<div className="pt-8">
+        <Container>
+           <Summary products={products} orders={orders} users={users} />
+        </Container>
+
+    </div>);
     
 }
 export default Admin;
